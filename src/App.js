09fa3fdc2802d581adb66c5e1e89778c68c09e90
Lutz <graham.lutz@adp.com>
2018-06-07 08:51:27 -0400

    levels 1 and 2 working - clean up still to do

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Grid from '@material-ui/core/Grid';
+import Zoom from '@material-ui/core/Zoom';
 import axios from 'axios';
 import logo from './logo.svg';
 import nodeLogo from './node.svg';
@@ -10,16 +18,98 @@ class App extends Component {
     super(props);
 
     this.state = {
-      page: 1
+      nextPage: 1,
+      clicked: false,
+      open: false
     }
   }
 
-  handleButtonClick = (e) => {
-    axios.get(`https://localhost:1701/users`)
-      .then(res => {
-        const persons = res.data.data;
-        this.setState({ persons });
+  componentDidMount() {
+    // window.addEventListener('scroll', this.handleScroll)
+  }
+
+  handleLetterFrequencyClick = (e) => {
+    this.setState((prevState) => {
+      let people = prevState.people;
+      let letterHash = {};
+      people.forEach((person) => {
+        person.email_address.split('').forEach((char) => {
+          if (!letterHash[char]) {
+            letterHash[char] = 1;
+          }
+          else {
+            letterHash[char]++
+          }
+        })
       })
+      return {
+        letterHash,
+        open: true
+      }
+    });
+  }
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
+  handleButtonClick = (e) => {
+    this.getPeople();
+  }
+
+  getPeople = () => {
+    let url = 'https://localhost:1701/users';
+    if (this.state.nextPage) {
+      url += '?page=';
+      url += this.state.nextPage;
+    }
+    this.setState({isLoadingItems: true})
+    axios.get(url).then(this.addPeople)
+  }
+
+  addPeople = (res) => {
+    this.setState((prevState) => {
+      let prevPeople = prevState.people || [];
+      let nextPeople;
+      let nextPage;
+      let people;
+
+      if (res.data && res.data.data) {
+        nextPeople = res.data.data;
+      }
+
+      if (res.data && res.data.metadata && res.data.metadata.paging && res.data.metadata.paging.next_page) {
+        nextPage = res.data.metadata.paging.next_page
+      }
+
+      people = prevPeople.concat(nextPeople);
+
+      return {
+        people,
+        nextPage,
+        clicked: true,
+        isLoadingItems: false
+      }
+    });
+  }
+
+  shouldLoadMoreRows = (scrollTop, gridBottom) => {
+    if (this.state.isLoadingItems) {
+      return false;
+    }
+
+    return gridBottom - scrollTop < 750;
+  };
+
+  handleScroll = (event) => {
+    const scrollTop = event.target.scrollTop;
+    const gridBottom = event.target.childNodes[0].clientHeight;
+    if (this.shouldLoadMoreRows(scrollTop, gridBottom)) {
+      this.setState({ isLoadingItems: true }, () => {
+        console.log('loading rows');
+        this.getPeople();
+      });
+    }
   }
 
   render() {
@@ -30,15 +120,72 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="react logo" />
           <h1 className="App-title">Graham Lutz's SalesLoft Challenge</h1>
         </header>
-        <p className="App-intro">
-          <Button variant="contained" color="primary" onClick={this.handleButtonClick}>
-            Click Me
-          </Button>
-        </p>
-        <div className="persons-container">
-        {this.state.persons && [...this.state.persons].map((person, idx) => {
-          return <p key={idx}>{person.email_address}</p>
-        })}
+        <div className="App-intro">
+          {
+            !this.state.clicked &&
+            <Button variant="contained" color="primary" onClick={this.handleButtonClick}>
+              Click Me
+            </Button>
+          }
+          {
+            this.state.clicked &&
+            <p>Scroll down for more</p>
+          }
+          <div>
+            <Button variant="contained" color="primary" onClick={this.handleLetterFrequencyClick}>
+                Get Letter Fequency
+            </Button>
+            <Dialog
+              fullScreen={false}
+              open={this.state.open}
+              onClose={this.handleClose}
+              aria-labelledby="responsive-dialog-title"
+            >
+              <DialogTitle id="responsive-dialog-title">{"Email Address Character Frequency"}</DialogTitle>
+              <DialogContent>
+                  {this.state.letterHash && Object.keys(this.state.letterHash).sort().map((letter, idx) => {
+                    return (
+                      <div key={idx}>
+                        <div className='letter-container'>{letter}</div>
+                        <div className='letter-container'>{this.state.letterHash[letter]}</div>
+                      </div>
+                    )
+                  })}
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={this.handleClose} color="primary" autoFocus>
+                  Close
+                </Button>
+              </DialogActions>
+            </Dialog>
+          </div>
+        </div>
+        <div className="persons-container" onScroll={this.handleScroll}>
+          <Grid container spacing={24}>
+            {
+              this.state.people && [...this.state.people].map((person, idx) => {
+              return (
+                <Grid item md key={idx}>
+                  <Zoom in={true}>
+                    <Card raised={true}>
+                      <div className='card-container'>
+                        <p>{person.first_name + ' ' + person.last_name}</p>
+                        <p>{person.title}</p>
+                        <p>{person.email_address}</p>
+                      </div>
+                    </Card>
+                  </Zoom>
+                </Grid>
+              )
+            })
+            }
+          </Grid>
+          {
+              this.state.isLoadingItems &&
+              <div className='progress-bar'>
+                <CircularProgress />
+              </div>
+            }
         </div>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -4,7 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-console.log(process.env.APIKEY)
-
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
